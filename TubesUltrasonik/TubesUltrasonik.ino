66330aa449c2a78a9dcb498fd9bf14279db93e11
andhikapp28 <andhika.119140224@student.itera.ac.id>
2021-12-29 06:28:01 +0700

    Fix Joystick code

--- TubesUltrasonik/TubesUltrasonik.ino
+++ TubesUltrasonik/TubesUltrasonik.ino
@@ -1,38 +1,68 @@
 #include <Servo.h>
 
-static const int servoPin = 4;
-const int trigPin = 12;
-const int echoPin = 13;
+// HCSR04
+#define trigPin 12
+#define echoPin 13
 long duration;
 float distanceCm;
 
+//Joystick
+int servoVal;
+#define joyX 27
+#define joyY 26
+#define sw 25
+
+//Button
+#define button 15
+bool statButton;
+
+//Tambahan
 #define SOUND_SPEED 0.034
 #define buzzer 23
 
 Servo servo1;
+Servo servo2;
 
 void setup() {
     Serial.begin(115200);
+
+    //Ultrasonik
     pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
     pinMode(echoPin, INPUT); // Sets the echoPin as an Input
     pinMode(buzzer, OUTPUT);
-    servo1.attach(servoPin);
+
+    //Servo
+    servo1.attach(4); //Bawah
+    servo2.attach(5); //Atas
+    servo2.write(170);
+
+    //Joy
+    pinMode(sw, INPUT_PULLUP);
+    pinMode(joyX, INPUT);
+    pinMode(joyY, INPUT);
+
+    //Button
+    pinMode(button, INPUT_PULLUP); //0 = True dan 1 = False
 }
 
 void loop() {
-    for(int posDegrees = 0; posDegrees <= 180; posDegrees++) {
-        servo1.write(posDegrees);
-        Serial.println(posDegrees);
-        ultrasonik();
-        delay(50);
-    }
-
-    for(int posDegrees = 180; posDegrees >= 0; posDegrees--) {
-        servo1.write(posDegrees);
-        Serial.println(posDegrees);
-        ultrasonik();
-        delay(50);
-    }
+    statButton = digitalRead(button);
+    if(statButton == 1){
+        for(int posDegrees = 0; posDegrees <= 180; posDegrees++) {
+          servo1.write(posDegrees);
+          Serial.println(posDegrees);
+          ultrasonik();
+          delay(50);
+        }
+        for(int posDegrees = 180; posDegrees >= 0; posDegrees--) {
+          servo1.write(posDegrees);
+          Serial.println(posDegrees);
+          ultrasonik();
+          delay(50);
+        }
+      }else{
+        joystick();
+      }
 }
 
 void ultrasonik(){
@@ -58,3 +88,19 @@ void ultrasonik(){
       Serial.println(distanceCm); 
     }
 }
+
+void joystick(){
+  Serial.println("Penggunaan Joystick");
+  servoVal = analogRead(joyX);
+  servoVal = map(servoVal, 0, 1023, 0, 180);
+  Serial.print("Sumbu X (Kiri-Kanan) : ");
+  Serial.println(servoVal);
+  servo1.write(servoVal);
+
+  servoVal = analogRead(joyY);
+  servoVal = map(servoVal, 0, 1023, 100, 180);
+  Serial.print("Sumbu Y (Atas-Bawah) : ");
+  Serial.println(servoVal);
+  servo2.write(servoVal);
+  delay(50);  
+  }
